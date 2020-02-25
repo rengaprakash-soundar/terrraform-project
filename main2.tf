2cb0fcfc6ed793d1f52ef6ff0aa7965ecde3d2d7
rengaprakash-soundar <59519532+rengaprakash-soundar@users.noreply.github.com>
2020-02-25 18:55:02 +0530

    mddd

--- main2.tf
+++ main2.tf
@@ -1,3 +1,9 @@
+
+provider "azurerm" {
+  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
+  version = "=2.0.0"
+  features {}
+}
 resource "azurerm_resource_group" "example" {
   name     =  var.resource_name
   location =  var.location
