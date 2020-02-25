658f48570af85d68e1555e8e21925f781e1c29fa
rengaprakash-soundar <59519532+rengaprakash-soundar@users.noreply.github.com>
2020-02-25 18:45:51 +0530

    my cha

--- main2.tf
+++ main2.tf
@@ -1,14 +1,14 @@
 resource "azurerm_resource_group" "example" {
-  name     = "${var.resource_name}"
-  location = "${var.location}"
+  name     =  var.resource_name
+  location =  var.location
 }
 
 # Create a virtual network within the resource group
 resource "azurerm_virtual_network" "example" {
-  name                = "${var.network_name}"
-  resource_group_name ="${var.resource_name}"
-  location            = "${var.location}"
-  address_space       ="${var.address_space}"
+  name                = var.network_name
+  resource_group_name = var.resource_name
+  location            = var.location
+  address_space       =var.address_space
 }
 
 
