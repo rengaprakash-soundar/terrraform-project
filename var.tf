92a15581cba4a79818fcb76d3d09057491f93166
rengaprakash-soundar <59519532+rengaprakash-soundar@users.noreply.github.com>
2020-02-25 18:47:33 +0530

    mycha

--- var.tf
+++ var.tf
@@ -1,16 +1,16 @@
 variable "resource_name" {
-  type =  "string"
+  type =  string
   default="samsunga133"
 }
 variable "location" {
-  type = "string"
+  type = string
   default="southindia"
 }
 variable "network_name" {
-  type = "string"
+  type = string
   default="samsunga133-vnet"
 }
 variable "address_space" {
-  type = "list"
+  type = list
   default=["192.168.0.0/16"]
 }
